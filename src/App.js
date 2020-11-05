5bf6d948e544745005ff30a8b489732c1d5832af
T_Tanya <tikhonovskyat@gmail.com>
2020-11-06 00:41:20 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import { Button } from 'react-bootstrap'
-
-const styles = {
-    text: {
-        fontSize: '50px',
-        fontFamily: 'sans-serif',
-        color: 'blue',
-        padding: '70px',
-        backgroundColor: '#11aaff'
-    },
-    button_container: {
-        marginTop: '200px',
-        textAlign: 'center'
-    },
-    button: {
-        backgroundColor: 'cyan',
-        border: 'none',
-        color: 'black',
-        fontSize: '24px',
-        height: '80px',
-        width: '120px',
-    }
-}
+import { Navbar, Button, Jumbotron, Row, Col } from 'react-bootstrap'
 
 class App extends Component
 {
@@ -45,13 +23,24 @@ class App extends Component
   {
     return (
       <div>
-        <div style={styles.text}>
-          <h1 align='center'>4 Lab React</h1>
-        </div>
-        <div style={styles.button_container}>
-          <button onClick={this.handleClick} style={styles.button}>Click Me</button>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            4 Lab React Bootstrap
+          </Navbar.Brand>
+        </Navbar>
+        <br />
+        <Row>
+          <Col></Col>
+          <Col md="auto">
+            <Button variant="primary" onClick={this.handleClick} >Block level button</Button>
+          </Col>
+          <Col xs lg="5"></Col>  
+        </Row>  
+        <br />
+        <Jumbotron>
+          <h2>Result</h2>
           <p>{this.state.id}</p>
-        </div>  
+        </Jumbotron>
       </div>
     )
     }
